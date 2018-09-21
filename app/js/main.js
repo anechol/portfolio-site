f6f89819645c3e58dfbddfd24ca32640ef1a659a
Ashley Echols <ashleyne92@gmail.com>
2018-09-21 17:08:21 -0500

    Remove personal projects and adds client work from current position.

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,58 @@
+import specimens1 from '../img/work/specimens1.png';
+import specimens2 from '../img/work/specimens2.png';
+import specimens3 from '../img/work/specimens3.png';
+import kellogg1 from '../img/work/kellogg1.png';
+import kellogg2 from '../img/work/kellogg2.png';
+import pcbasics1 from '../img/work/pcbasics1.png';
+import pcbasics2 from '../img/work/pcbasics2.png';
+import pcbasics3 from '../img/work/pcbasics3.png';
+
 const projects = [
   {
-    title: "name",
-    description: "description",
-    image: "image",
-    imageAlt: "imagealt",
-    tags: ["tag1", "tag2", "tag3"]
+    title: "Specimens Monologues",
+    info: "info",
+    duties: "sttttuuuffff",
+    image: [
+      specimens1,
+      specimens2,
+      specimens3
+    ],
+    imageAlt: [
+      "Home Page",
+      "List of Specimens",
+      "Monologue Text Overlay"
+    ],
+    tags: ["HTML5/CSS3", "Sass", "React"]
   },
   {
-    title: "name",
-    description: "description",
-    image: "image",
-    imageAlt: "imagealt",
-    tags: ["tag1", "tag2", "tag3", "tag4"]
+    title: "Kellogg's",
+    info: "info",
+    duties: "sttttuuuffff",
+    image: [
+      kellogg1,
+      kellogg2
+    ],
+    imageAlt: [
+      "Rice Krispies Home Page",
+      "Eggo Waffles Home Page"
+    ],
+    tags: ["CSS", "Sass", "AEM"]
   },
   {
-    title: "name",
-    description: "description",
-    image: "image",
-    imageAlt: "imagealt",
+    title: "Intel PC Basics",
+    info: "info",
+    duties: "sttttuuuffff",
+    image: [
+      pcbasics1,
+      pcbasics2,
+      pcbasics3
+    ],
+    imageAlt: [
+      "Home Page",
+      "blah1",
+      "blah2",
+      "blah3",
+    ],
     tags: ["tag1", "tag2"]
   }
 ];
@@ -27,31 +62,43 @@ const renderWork = () => {
                   <div class='row'>
                     <div class='title twelve columns-sm'>
                       <h2>Projects</h2>
-                    </div>`;
+                    </div>
+                    <div class='flexbox twelve columns-sm'>`;
   
   projects.forEach(el => {
+
+    function getImages() {
+      let markup = '';
+      for (let i = 0; i < el.image.length; i++) {
+        markup+= `<img src=${el.image[i]} alt='${el.imageAlt[i]}'/>`
+      }
+      return markup;
+    }
 
     function getListItem() {
       let markup = '';
       for (let i = 0; i < el.tags.length; i++) {
-        markup+= "<li>" + el.tags[i] + "</li>";
+        markup+= `<li>${el.tags[i]}</li>`;
       }
       return markup;
     }
 
     markup += `
-    <div class="box">
-      <h4>Title: ${el.title}</h4>
-      <img src=${el.image} alt=${el.imageAlt}>
-      <p>Description: ${el.description}</p>
-      <ul class='tags'>
-        ${getListItem()}
-      </ul>
+    <div class="item">
+      <h4>${el.title}</h4>
+      ${getImages()}
+      <div class='info'>
+        <p><strong>The Project:</strong> ${el.info}</p>
+        <p><strong>Responsibilities:</strong> ${el.duties}</p>
+        <ul class='tags'>
+          ${getListItem()}
+        </ul>
+      </div>
     </div>
     `
   });  
 
-  markup += `</div></div>`
+  markup += `</div></div></div>`
 
   document.getElementById("work").innerHTML = markup;
 }
